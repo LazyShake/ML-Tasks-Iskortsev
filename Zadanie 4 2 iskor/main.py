35691cb24c9000c59991fd5892aaa7e812e69833
LazyShake <sashaoligator@gmail.com>
2022-10-27 20:56:20 +0700

    5 zadanie

--- Zadanie 4 2 iskor/main.py
+++ Zadanie 4 2 iskor/main.py
@@ -14,36 +14,36 @@ def print_hi(name):
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm')
-Number = -5238
+Number = 2147483649645674869
 
 
 print(Number)
 Length = 0
-if Number < 2147483647 and Number > -2147483648:
-    if Number < 0:
-        Number = Number * -1
-        TempNumber = Number
+if Number < 0:
+    Number = Number * -1
+    TempNumber = Number
 
-        while TempNumber != 0:
-            Length += 1
-            TempNumber = TempNumber // 10
-        NewNumber = 0
-        for i in range(Length):
-            NewNumber = NewNumber + (Number % 10) * 10 ** (Length - 1 - i)
-            Number = Number // 10
-        NewNumber = NewNumber * -1
-    else:
-        TempNumber = Number
-        while TempNumber != 0:
-            Length += 1
-            TempNumber = TempNumber // 10
-        NewNumber = 0
-        for i in range(Length):
-            NewNumber = NewNumber + (Number % 10) * 10 ** (Length - 1 - i)
-            Number = Number // 10
-else:
+    while TempNumber != 0:
+        Length += 1
+        TempNumber = TempNumber // 10
     NewNumber = 0
+    for i in range(Length):
+        NewNumber = NewNumber + (Number % 10) * 10 ** (Length - 1 - i)
+        Number = Number // 10
+    NewNumber = NewNumber * -1
+else:
+    TempNumber = Number
+    while TempNumber != 0:
+        Length += 1
+        TempNumber = TempNumber // 10
+    NewNumber = 0
+    for i in range(Length):
+        NewNumber = NewNumber + (Number % 10) * 10 ** (Length - 1 - i)
+        Number = Number // 10
 
 
+
+if NewNumber > 2147483647 or NewNumber < -2147483648:
+    NewNumber = 0
 print(NewNumber)
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
